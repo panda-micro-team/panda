20a8a1e142a946894bd49a49442cd58715d4cee1
IvanKharkivskyi1 <90053847+IvanKharkivskyi1@users.noreply.github.com>
2024-09-21 15:22:49 +0300

    added counter

---
+++ app.js
@@ -0,0 +1,22 @@
+const Counter = () => {
+    const [count, setCount] = React.useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        setCount(count - 1);
+    };
+
+    return (
+        <div>
+            <h1>React Counter</h1>
+            <div className="count">{count}</div>
+            <button onClick={decrement}>-</button>
+            <button onClick={increment}>+</button>
+        </div>
+    );
+};
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
