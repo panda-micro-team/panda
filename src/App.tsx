0755c1f4c351289e1613e9bee1532ea37d0ac640
IvanKharkivskyi1 <ikcomua@gmail.com>
2024-11-26 15:48:24 +0200

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { Counter, Dashboard, Footer, Navbar } from "./components";
 import { ThemeProvider } from "./contexts/ThemeContext/ThemeContext";
 import { Flex } from "@chakra-ui/react";
-import { useEffect } from "react";
 
 const App = () => {
   return (
@@ -11,23 +10,13 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<DashboardRedirect />} />
           <Route path="/counter" element={<Counter />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
       </Flex>
     </ThemeProvider>
   );
-};
-
-const DashboardRedirect = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/", { replace: true });
-  }, [navigate]);
-
-  return null;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
-  const isProduction = process.env.NODE_ENV === "production";
-  const basename = isProduction ? "/panda" : "/";
-
   root.render(
     <React.StrictMode>
-      <BrowserRouter basename={basename}>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>,
   );
 }
